07306c95f88afa9807e257ca1ca7a6b9d51006a2
Miguel S Mendoza <miguel@smendoza.net>
2016-07-20 11:54:46 +0000

    Final

--- server.js
+++ server.js
@@ -1,84 +1,32 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
-
-var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
+var app = express();
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
-
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
-
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+app.get('/:time', function (req, res) {
+  var time = req.params.time;
+  res.send({"unix":parseUnix(time), "natural":parseDate(time)});
 });
+
+app.listen(8080, function () {
+  console.log('Timestamp Server listening on port 8080!');
+});
+
+function parseDate(value) {
+    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    if(isNaN(value)) {
+      var date = new Date(Date.parse(value));
+    } else {
+      var date = new Date(parseInt(value*1000));
+      value = monthNames[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear();
+    }
+    return (date) ? value : null;
+}
+
+function parseUnix(value) {
+    if(!isNaN(value)) {
+      var date = value;
+    } else {
+      var date = new Date(Date.parse(value));
+      date = date.getMilliseconds()/1000;
+    }
+    return (date) ? date : null;
+}
