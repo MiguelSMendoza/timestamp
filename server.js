a9438785978ebb028f8e701f68dd2e9731c81d87
Miguel S Mendoza <miguel@smendoza.net>
2016-07-20 17:19:29 +0000

    Last Commit

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
 var express = require('express');
 var app = express();
 
-app.get('/:time', function (req, res) {
+app.get('/:time', function(req, res) {
   var time = req.params.time;
-  res.send({"unix":parseUnix(time), "natural":parseDate(time)});
+  res.send({
+    "unix": parseUnix(time),
+    "natural": parseDate(time)
+  });
 });
 
-app.listen(8080, function () {
+app.listen(8080, function() {
   console.log('Timestamp Server listening on port 8080!');
 });
 
 function parseDate(value) {
-    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    if(isNaN(value)) {
-      var date = new Date(Date.parse(value));
-    } else {
-      var date = new Date(parseInt(value)*1000);
-    }
-    return checkDate(date) ? monthNames[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear() : null;
+  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var date = null;
+  if (isNaN(value)) {
+    date = new Date(Date.parse(value));
+  }
+  else {
+    date = new Date(parseInt(value) * 1000);
+  }
+  return checkDate(date) ? monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear() : null;
 }
 
 function parseUnix(value) {
-    if(!isNaN(value)) {
-      var date = new Date(parseInt(value)*1000);
-    } else {
-      var date = new Date(Date.parse(value));
-    }
-    return checkDate(date) ? date.getTime()/1000 : null;
+  var date = null;
+  if (!isNaN(value)) {
+    date = new Date(parseInt(value) * 1000);
+  }
+  else {
+    date = new Date(Date.parse(value));
+  }
+  return checkDate(date) ? date.getTime() / 1000 : null;
 }
 
 function checkDate(date) {
